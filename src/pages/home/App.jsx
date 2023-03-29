2a49f691d1b09edf85dcfb141a364107f83a6821
Nweke Chidi <nwekechidigodwin460@gmail.com>
2023-03-29 04:32:14 +0100

    setup folder structure
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

---
+++ src/pages/home/App.jsx
@@ -0,0 +1,15 @@
+import "../../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="App-link text-8xl font-bold underline">
+          Welcome to transit flow by chidi
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
