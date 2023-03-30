81bdb68c0e6fb1af4e0bb2996b84fc1532e11bf9
Nweke Chidi <nwekechidigodwin460@gmail.com>
2023-03-30 21:47:05 +0100

    add footer image
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import FooterImage from "../../assets/images/FooterImage.png";
 
 function Footer() {
   return (
    <footer>
-    I'm the footer
+    <img src={FooterImage} alt="Footer Container" />
    </footer>
   )
 }
